aa6f3fa0b6e33d368c64165da50589401d824a38
lky1020 <57024183+lky1020@users.noreply.github.com>
2021-04-23 09:16:53 +0800

    Update Module && Add Brightness Control

---
+++ BrightnessHandControl.py
@@ -0,0 +1,60 @@
+import cv2
+import time
+import numpy as np
+import HandTrackingModule as htm
+import math
+import screen_brightness_control as sbc
+
+wCam, hCam = 640, 480
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+pTime = 0
+
+detector = htm.handDetector(detectionCon=0.8)
+
+minBright = 0
+maxBright = 100
+brightness = 0
+brightnessBar = 400
+brightnessPer = 0
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+
+    if len(lmList) != 0:
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        cv2.circle(img, (x1, y1), 10, (0, 255, 0), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 10, (0, 255, 0), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
+        cv2.circle(img, (cx, cy), 5, (0, 0, 255), cv2.FILLED)
+
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        brightness = np.interp(length, [35, 275], [minBright, maxBright])
+        brightnessBar = np.interp(length, [35, 275], [400, 150])
+        brightnessPer = np.interp(length, [35, 275], [0, 100])
+        print(int(brightness))
+
+        sbc.set_brightness(int(brightness))
+
+        if length < 35:
+            cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+
+    cv2.rectangle(img, (50, 150), (85, 400), (0, 255, 0), 3)
+    cv2.rectangle(img, (50, int(brightnessBar)), (85, 400), (0, 255, 0), cv2.FILLED)
+    cv2.putText(img, F'{int(brightnessPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, F'fps: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
+
+    cv2.imshow("Img", img)
+    cv2.waitKey(1)
--- FingerCounting.py
+++ FingerCounting.py
@@ -2,6 +2,11 @@ import cv2
 import time
 import os
 import HandTrackingModule as htm
+import numpy as np
+import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 wCam, hCam = 640, 480
 
@@ -24,48 +29,100 @@ prevTime = 0
 
 detector = htm.handDetector(detectionCon=0.75)
 
+# Finger Counting
 tipIds = [4, 8, 12, 16, 20]
+
+# Volume Control
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+volRange = volume.GetVolumeRange()
+minVol = volRange[0]
+maxVol = volRange[1]
+vol = 0
+volBar = 400
+volPer = 0
+
+key = ''
 
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
 
+    if cv2.waitKey(1) & 0xFF == ord('f'):
+        key = 'f'
+
+    elif cv2.waitKey(1) & 0xFF == ord('v'):
+        key = 'v'
+
     if len(lmList) != 0:
-        fingers = []
 
-        # thumb
-        if lmList[5][1] > lmList[17][1]:
-            #  Right
-            # [index finger][height]
-            if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][2] + 100:
-                fingers.append(1)
+        if key == 'f':
+            # Finger Counting
+            fingers = []
+
+            # thumb
+            if lmList[5][1] > lmList[17][1]:
+                #  Right
+                # [index finger][height]
+                if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][2] + 100:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
             else:
-                fingers.append(0)
-        else:
-            # Left
-            # [index finger][height]
-            if lmList[tipIds[0]][1] < lmList[tipIds[0] - 1][2]:
-                fingers.append(1)
-            else:
-                fingers.append(0)
+                # Left
+                # [index finger][height]
+                if lmList[tipIds[0]][1] < lmList[tipIds[0] - 1][2]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
 
-        print("Index 4 = " + str(lmList[tipIds[0]][1]))
-        print("Index 3 = " + str(lmList[tipIds[0] - 1][2]))
+            print("Index 4 = " + str(lmList[tipIds[0]][1]))
+            print("Index 3 = " + str(lmList[tipIds[0] - 1][2]))
 
-        # 4 fingers
-        for id in range(1, 5):
-            # [index finger][height]
-            if lmList[tipIds[id]][2] < lmList[tipIds[id]-2][2]:
-                fingers.append(1)
-            else:
-                fingers.append(0)
+            # 4 fingers
+            for id in range(1, 5):
+                # [index finger][height]
+                if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
 
-        totalFingers = fingers.count(1)
-        print(totalFingers)
+            totalFingers = fingers.count(1)
+            print(totalFingers)
 
-        h, w, c = overlayList[totalFingers-1].shape
-        img[0:h, 0:w] = overlayList[totalFingers-1]
+            h, w, c = overlayList[totalFingers - 1].shape
+            img[0:h, 0:w] = overlayList[totalFingers - 1]
+
+        if key == "v":
+            x1, y1 = lmList[4][1], lmList[4][2]
+            x2, y2 = lmList[8][1], lmList[8][2]
+            cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+            cv2.circle(img, (x1, y1), 10, (0, 255, 0), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 10, (0, 255, 0), cv2.FILLED)
+            cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
+            cv2.circle(img, (cx, cy), 5, (0, 0, 255), cv2.FILLED)
+
+            length = math.hypot(x2 - x1, y2 - y1)
+
+            # Hand Range 35 - 275
+            # volume range -65 - 0
+
+            vol = np.interp(length, [35, 275], [minVol, maxVol])
+            volBar = np.interp(length, [35, 275], [400, 150])
+            volPer = np.interp(length, [35, 275], [0, 100])
+            print(vol)
+
+            volume.SetMasterVolumeLevel(vol, None)
+
+            if length < 35:
+                cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+
+            cv2.rectangle(img, (50, 150), (85, 400), (0, 255, 0), 3)
+            cv2.rectangle(img, (50, int(volBar)), (85, 400), (0, 255, 0), cv2.FILLED)
+            cv2.putText(img, F'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
 
     currentTime = time.time()
     fps = 1 / (currentTime - prevTime)
@@ -74,4 +131,6 @@ while True:
     cv2.putText(img, f'FPS: {int(fps)}', (400, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
 
     cv2.imshow("img", img)
-    cv2.waitKey(1)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
